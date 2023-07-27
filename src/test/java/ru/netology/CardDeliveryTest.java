a25cc5f3e77f39416f192551eb21d80a8f1280c3
ElenaKr <Kravets_2024@internet.ru>
2023-07-27 10:13:23 +0500

    Modification tests

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -6,10 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.util.Calendar;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.Duration;
-import java.util.GregorianCalendar;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.withText;
@@ -18,6 +17,9 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @BeforeEach
     void setUp() {
@@ -27,55 +29,53 @@ public class CardDeliveryTest {
     @Test
     void shouldHappyPathTest() {
 
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3); // увеличиваем на 3 дня от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(c.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
+        String planningDate = generateDate(3);
+//        Calendar c = new GregorianCalendar();
+//        c.add(Calendar.DAY_OF_YEAR, 3); // увеличиваем на 3 дня от текущей даты
+//        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
+//        String date = format1.format(c.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("Казань");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Иван Соколов");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate),
+                Duration.ofSeconds(15)).shouldBe(Condition.visible);
+
     }
 
     @Test
     void shouldMistakeCity() {
 
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3); // увеличиваем на 3 дня от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(c.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
-
+        String planningDate = generateDate(3);
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("казань");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Иван Соколов");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate),
+                Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
     void shouldLatinCity() {
 
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3); // увеличиваем на 3 дня от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(c.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
-
+        String planningDate = generateDate(3);
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("Moscow");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Иван Соколов");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$("[data-test-id=agreement]").click();
@@ -86,16 +86,12 @@ public class CardDeliveryTest {
     @Test
     void shouldAnotherCity() {
 
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3); // увеличиваем на 3 дня от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(c.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
-
+        String planningDate = generateDate(3);
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("Златоуст");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Иван Соколов");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$("[data-test-id=agreement]").click();
@@ -106,17 +102,12 @@ public class CardDeliveryTest {
     @Test
     void shouldDateBeforeLimit() {
 
-        Calendar cal = new GregorianCalendar();
-        cal.add(Calendar.DAY_OF_YEAR, 1); // увеличиваем на 1 дня от текущей даты
-
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(cal.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
-
+        String planningDate = generateDate(1);
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("Казань");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Иван Соколов");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$("[data-test-id=agreement]").click();
@@ -127,36 +118,30 @@ public class CardDeliveryTest {
     @Test
     void shouldDateOverLimit() {
 
-        Calendar cal = new GregorianCalendar();
-        cal.add(Calendar.DAY_OF_YEAR, 4); // увеличиваем на 4 дня от текущей даты
-
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(cal.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
-
+        String planningDate = generateDate(4);
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("Казань");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Иван Соколов");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate),
+                Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
     void shouldLatinNameTest() {
 
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3); // увеличиваем на 3 дня от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(c.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
+        String planningDate = generateDate(3);
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("Казань");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Ivan Sokolov");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$("[data-test-id=agreement]").click();
@@ -167,34 +152,30 @@ public class CardDeliveryTest {
     @Test
     void shouldNameWithDashTest() {
 
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3); // увеличиваем на 3 дня от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(c.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
+        String planningDate = generateDate(3);
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("Казань");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Анна-Мария Смирнова");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate),
+                Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
     void shouldValidationCheckbox() {
 
-        Calendar c = new GregorianCalendar();
-        c.add(Calendar.DAY_OF_YEAR, 3); // увеличиваем на 3 дня от текущей даты
-        SimpleDateFormat format1 = new SimpleDateFormat("dd.MM.yyyy"); //придаем нужный формат дате
-        String date = format1.format(c.getTime());//вытягиваем измененную дату в нужном формате и присваиваем переменной
+        String planningDate = generateDate(3);
 
         SelenideElement form = $(".form, .form_size_m, .form_theme_alfa-on-white");
         form.$("[data-test-id=city] input").setValue("Казань");
         form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
-        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Анна-Мария Смирнова");
         form.$("[data-test-id=phone] input").setValue("+79092364751");
         form.$(".button").click();
